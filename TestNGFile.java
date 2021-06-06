f6596fa78557a98d9ff201a3f6eb836ca2704f18
Lee Walsh <85432998+lpwalsh1992@users.noreply.github.com>
2021-06-06 17:14:01 +0100

    Update TestNGFile.java

--- TestNGFile.java
+++ TestNGFile.java
@@ -1,20 +1,21 @@
 package BSProject;
 
-import static org.testng.Assert.assertNotNull;
+
 import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 
-import java.util.ArrayList;
+import java.awt.AWTException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
 public class TestNGFile {
-	public String defaultPage = "https://login.yahoo.com/";
+	public String defaultPage = "https://www.nufc.co.uk/";
 	String driverPath = "C:\\Selenium\\chromedriver_win32\\chromedriver.exe";
 	public WebDriver driver;
 	
@@ -25,43 +26,37 @@ public class TestNGFile {
 	}
 	
 	@BeforeTest
-    public void launchBrowser() {
+    public void launchBrowser() throws InterruptedException, AWTException {
         System.out.println("Launching browser..."); 
         System.setProperty("webdriver.chrome.driver", driverPath);
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        // options.addArguments("");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get(defaultPage);
+        Thread.sleep(2000);
     }
 	
   @Test(priority = 0)
-  public void verifyCheckbox() {
-		WebElement checkbox = driver.findElement(By.id("persistent"));
-		Boolean verifyCheckbox = checkbox.isSelected();
-		assertTrue(verifyCheckbox);
+  public void verifyBanner() {
+		WebElement BannerInfo = driver.findElement(By.className("banner__content"));
+		Boolean verifyBanner = BannerInfo.isDisplayed();
+		assertTrue(verifyBanner);
   }
   @Test(priority = 1)
-  public void verifyPrivacyPage() {
-		driver.findElement(By.id("createacc")).click();
-		driver.findElement(By.className("termsLink")).click();
-		String myWindowHandle = driver.getWindowHandle();
-		driver.switchTo().window(myWindowHandle);
-		driver.findElement(By.className("privacyLink")).click();
-		driver.close();
-		ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
-	    driver.switchTo().window(tabs2.get(1));
-	    driver.close();
-	    driver.switchTo().window(tabs2.get(0));
-	    String actualTitle = (driver.getTitle());
-	    String expectedTitle = "Verizon Media Terms of Service | Verizon Media Policies";
-	    assertEquals(expectedTitle,actualTitle);
+  public void verifySocialLinks() {
+		WebElement socialLinks = driver.findElement(By.className("social-links"));
+		Boolean verifySocial = socialLinks.isDisplayed();
+		assertTrue(verifySocial);
   }
   
   @Test(priority = 2)
-  public void verifyNavPane() {
-  WebElement sidebar = driver.findElement(By.id("site-sidebar"));
-  assertNotNull(sidebar);
+  public void verifyNUFCTV() {
+	  driver.get("https://www.nufc.co.uk/nufc-tv/");
+	  String actualTitle = driver.getTitle();
+	  String expectedTitle = "Newcastle United - NUFC TV";
+	  assertEquals(expectedTitle, actualTitle);
 }
 }
